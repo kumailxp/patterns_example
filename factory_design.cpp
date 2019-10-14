9c82194991026e67391b5c40bff0b660479f7c37
Kumail Ahmed <kumail89@gmail.com>
2019-10-15 00:41:16 +0200

    impl several patterns

--- factory_design.cpp
+++ factory_design.cpp
@@ -2,10 +2,25 @@
 #include <math.h>
 #include <sstream>
 #include <string>
+#include <memory>
+#include <vector>
+#include <functional>
+
+struct Point;
+
+enum PrinterType {
+  html,
+  markdown
+};
 
 struct Point {
   float x, y;
   friend class PointFactory;
+
+  std::string str() {
+    return std::string("x= ") + std::to_string(x)
+          + ", y=" + std::to_string(y) + "\n";
+  }
 
 private:
   Point(float x, float y) : x(x), y(y) {}
@@ -16,10 +31,55 @@ struct PointFactory {
     { return Point{x, y}; }
 
   static Point NewPolar(float r, float theata) {
-    return Point{r * sin(theata), r * cos(theata)};
+    return Point {r * sin(theata), r * cos(theata)};
+  }
+};
+
+
+struct PrintingStrategy {
+  virtual void output1(std::string str)= 0;
+};
+
+struct HtmlPrinting : PrintingStrategy {
+  void output1(std::string str) override;
+};
+
+void HtmlPrinting::output1(std::string str) {
+  std::cout << "html output: " << str;
+}
+
+
+struct MarkdownPrinting : PrintingStrategy {
+  void output1(std::string str) override {
+    std::cout << "markdown output: " << str;
+  }
+};
+
+struct PointProcessor : std::vector<Point> {
+private:
+  PrintingStrategy* ps_;
+public:
+  PointProcessor(const std::initializer_list<value_type>& points,
+    PrintingStrategy* ps) :
+    std::vector<Point>(points),
+    ps_(ps) {}
+  void print() {
+    for (Point &I : *this)
+      ps_->output1(I.str());
+  }
+
+  ~PointProcessor() {
+    delete ps_;
   }
 };
 
 int main() {
-  auto myPoint = PointFactory::NewCartesian(3,4);
+  auto pp = PointProcessor {{ 
+        PointFactory::NewPolar(10,3.142),
+        PointFactory::NewCartesian(34,52),
+        PointFactory::NewCartesian(10,3100)
+   },
+    new HtmlPrinting()
+   };
+  pp.print();
 }
